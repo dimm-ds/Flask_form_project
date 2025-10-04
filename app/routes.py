5e631aa1e49c4005c5b126add4113005049e62a1
dimm-ds <dimm_ds@mail.ru>
2025-10-05 01:27:50 +0500

    Initial commit try2 Правка конструктора класса

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, redirect, url_for
+from app import app
+
+
+@app.route("/")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        name = request.form.get("name")  # Получаем имя из формы
+        color = request.form.get("color")
+        profession = request.form.get("profession")
+        hobbies = request.form.getlist("hobbies")  # Для чекбоксов
+        level = request.form.get("level")
+        email = request.form.get("email")  # Получаем email из формы
+        return render_template("result.html", name=name, color=color, profession=profession, hobbies=hobbies,
+                               level=level, email=email)
+    else:
+        return redirect(url_for("form"))  # Если запрос GET, возвращаем на форму
